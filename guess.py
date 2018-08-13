7cfe6470408cc429ce2c778c4db1c227ae15892d
Hurt Huang <hurt.nzsmr@gmail.com>
2018-08-13 22:53:12 +0800

    ad range

--- guess.py
+++ guess.py
@@ -1,11 +1,16 @@
 import random
-r = random.randint(1, 100)
+s = input('請輸入最小值：')
+s = int(s)
+e = input('請輸入最大值：')
+e = int(e)
+r = random.randint(s, e)
 t = 0
+print('範圍(', s, '~', e, ')')
 while True:
-	num = input('請猜數字(1~100)：')
+	num = input('請猜數字：')
 	num = int(num)
 	if num == r:
-		print('恭禧答對了！')
+		print('恭禧答對了！共答了', t + 1, '次')
 		break
 	else:
 		if num > r:
